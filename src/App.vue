e690f31eee9c0c8d31af57c27e544334c3fdc95b
Keremix © <aske2005@hotmail.com>
2017-05-09 21:55:17 +0300

    Aske Karate Spor Kulübü Kurulum Dosyaları

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    
+    <nav>
+       <router-link to="/Home">Home Page</router-link>
+       <router-link to="/Athlets">Athlets</router-link>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Athlets.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>{{title}}
+</template>
+
+<script>
+export default{
+  name: 'athlets',
+  data () {
+    return{
+        title:'Athlets Page'
+    }
+  }
+
+}
+</script>
